f98f1a87bbe598ba9c54b69c7012d7dc446dd7a2
rifaiabdurrohman <rifaiabdurrohman8@gmail.com>
2025-09-18 12:38:01 +0700

    feat : connext db

--- GameStore/Program.cs
+++ GameStore/Program.cs
@@ -1,6 +1,10 @@
+using GameStore.Data;
 using GameStore.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSqlite<GameStoreContext>("Data Source=GameStore.db");
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
